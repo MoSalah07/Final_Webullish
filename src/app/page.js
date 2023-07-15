4aebcb566e640168116c060f0df3e58755f0ac38
MoSalah07 <notway10@yahoo.com>
2023-07-15 16:59:16 +0300

    change_for_anas

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ import Login from './components/login/Login';
 export default function LoginScreen() {
   return (
     <section className='bg-primary-white max-h-[90vh] '>
-      <h1>welcome back</h1>
+      <h1>other change for my brother anas</h1>
       <Login />
     </section>
   )
