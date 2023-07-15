14a2479b04f487af496e4b0e8ba690163cea725c
MoSalah07 <notway10@yahoo.com>
2023-07-15 16:42:45 +0300

    other_change

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ import Login from './components/login/Login';
 export default function LoginScreen() {
   return (
     <section className='bg-primary-white max-h-[90vh] '>
-      <h1>Hello Mohamed Salah</h1>
+      <h1>welcome back</h1>
       <Login />
     </section>
   )
